fc85b9c92f17b40059c84b1455a12cc90433c422
Kulwinder Kohli <info@iamkulwinderkohli.com>
2020-11-20 15:58:21 +0530

    - admin panel integration

---
+++ src/Layout/Layout.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+class Layout extends Component {
+	render() {
+		return (
+			<div>
+				<div id="wrapper">
+					<Sidebar/>
+						<div id="content-wrapper" className="d-flex flex-column">
+		            		<div id="content">
+								<Header/>
+							</div>
+							<Footer/>
+						</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Layout
