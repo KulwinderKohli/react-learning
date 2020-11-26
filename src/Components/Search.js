69b9dd0f9a5fa17b517c9c2f1f4511b6a82c311e
Kulwinder Kohli <info@iamkulwinderkohli.com>
2020-11-26 16:10:21 +0530

    - session 4
    States
    Packages

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -5,8 +5,15 @@ class Search extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			name: "Kulwinder"
+			name: ""
 		}
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(e) {
+		this.setState({
+			name: e.target.value
+		})
 	}
 
 	render() {
@@ -19,7 +26,7 @@ class Search extends Component {
                 <div className="input-group">
                 	{/* Placeholder data */}
                     <input type="text" className="form-control bg-light border-0 small" placeholder={placeholder}
-                        aria-label="Search" aria-describedby="basic-addon2"/>
+                        aria-label="Search" aria-describedby="basic-addon2" onChange={this.handleChange}/>
                     <div className="input-group-append">
                         <button className={"btn btn-"+btnclass} type="button">
                             <i className="fas fa-search fa-sm"></i>
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
+import { Container, Button, ProgressBar, Row, Col } from 'react-bootstrap';
+
+// Datepicker package
+import DatePicker from "react-datepicker";
+import "../Style/Datepicker.css";
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import es from 'date-fns/locale/es';
 
 class Layout extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date()
+		};
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(date) {
+		console.log(date)
+		this.setState({
+			date: date
+		})
+	}
+
 	render() {
+		const { date } = this.state
 		return (
 			<div>
 				<div id="wrapper">
@@ -12,7 +35,20 @@ class Layout extends Component {
 						<div id="content-wrapper" className="d-flex flex-column">
 		            		<div id="content">
 								<Header/>
-								
+								<Container>
+									<Row>
+										<Col>
+											<ProgressBar animated now={45} />
+											<br/>
+											<DatePicker
+												locale="es"
+												selected={date}
+												onChange={this.handleChange}
+											/>
+											<Button variant="outline-success" size="sm">My First Button</Button>
+										</Col>
+									</Row>
+								</Container>
 							</div>
 							<Footer text="Copyright OneBusiness"/>
 						</div>
