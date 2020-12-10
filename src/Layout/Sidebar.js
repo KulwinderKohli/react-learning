e91f4cae8147d33de1ccc8cf58b98608bfa1869c
Kulwinder Kohli <info@iamkulwinderkohli.com>
2020-12-10 13:30:56 +0530

    - sidebar

--- src/Layout/Sidebar.js
+++ src/Layout/Sidebar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import MenuItem from '../Components/MenuItem'
 import { BrowserRouter as Router, Link } from "react-router-dom"
+import { Accordion } from 'react-bootstrap'
+
+import '../Style/Sidebar.css'
 
 class Sidebar extends Component {
 
@@ -28,18 +31,35 @@ class Sidebar extends Component {
                 <MenuItem menuname="Images" menuicon="fas fa-fw fa-images" route="images" />
                 <MenuItem menuname="Posts" menuicon="fas fa-fw fa-file-alt" route="posts" />
                 <li className="nav-item">
-                <Link className="nav-link collapsed" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages" to="employee">
-                    <i className="fas fa-fw fa-users"></i>
-                    <span>Employee</span>
-                </Link>
-                <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <a className="collapse-item">All Employees</a>
-                        <a className="collapse-item">Add Employee</a>
-                    </div>
-                </div>
-            </li>
+                    <Accordion>
+                        <Accordion.Toggle as={Link} className ="nav-link dropdown" variant="link" eventKey="0">
+                            <i className="fas fa-fw fa-users"></i>
+                            <span>Employee</span>
+                        </Accordion.Toggle>
+
+                        <Accordion.Collapse eventKey="0">
+                            <div className="bg-white py-2 collapse-inner rounded">
+                                    <a className="collapse-item">All Employees</a>
+                                    <a className="collapse-item">Add Employee</a>
+                            </div>
+                        </Accordion.Collapse>
+                    </Accordion>
+                </li>
+                <li className="nav-item">
+                    <Accordion>
+                            <Accordion.Toggle as={Link} className ="nav-link dropdown" variant="link" eventKey="1">
+                                <i className="fas fa-fw fa-users"></i>
+                                <span>Employee</span>
+                            </Accordion.Toggle>
+
+                            <Accordion.Collapse eventKey="1">
+                                <div className="bg-white py-2 collapse-inner rounded">
+                                        <a className="collapse-item">All Employees</a>
+                                        <a className="collapse-item">Add Employee</a>
+                                </div>
+                            </Accordion.Collapse>
+                    </Accordion>
+                </li>
                 <hr className="sidebar-divider d-none d-md-block" />
 
             </ul>
